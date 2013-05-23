937d02e41ae9b93fb365336d18db022e74887a7b
Stephane Maldini <smaldini@gopivotal.com>
2013-05-23 18:19:15 +0100

    Move Builder logic into generic reactor.config.Builder, add building for Reactor

--- reactor-core/src/main/java/reactor/support/Assert.java
+++ reactor-core/src/main/java/reactor/support/Assert.java
@@ -21,7 +21,7 @@ package reactor.support;
  * @author Stephane Maldini (smaldini)
  * @date: 3/21/13
  *
- * Port the assertion logic from spring-beans to avoid dependency
+ * Port the assertion logic when spring-beans to avoid dependency
  */
 public class Assert {
 
